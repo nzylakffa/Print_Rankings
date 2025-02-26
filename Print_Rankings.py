781a7c14f104b7b8bddcaf0c8d39cc3c4521d24e
nzylakffa <131708217+nzylakffa@users.noreply.github.com>
2025-02-26 15:16:54 -0500

    Update Print_Rankings.py

--- Print_Rankings.py
+++ Print_Rankings.py
@@ -52,7 +52,7 @@ def generate_pdf(title, df, last_updated, is_top_200=False):
     
     # ✅ Add Image (Top Right Corner)
     image_path = "ffa_red.png"  # Local path
-    pdf.image(image_path, x=161, y=-2, w=35)  # Adjust X, Y, and width as needed
+    pdf.image(image_path, x=155, y=-2, w=35)  # Adjust X, Y, and width as needed
 
     # ✅ Sub-header: Last Updated (Smaller Font, Only for Full Rankings)
     if not is_top_200:
